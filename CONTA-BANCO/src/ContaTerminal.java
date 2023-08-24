17cf993734077c768457acb866ce0228c311fcf0
bdiasr95 <bdiasr95@gmail.com>
2023-08-24 20:45:05 -0300

    projeto conta banco concluida

--- CONTA-BANCO/src/ContaTerminal.java
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Bem-vindo ao Banco Gringotes !!!");
+        System.out.println("Qual o seu nome?");
+        String Name = sc.nextLine();
+        
+        System.out.println("Por favor, digite o número da sua Agência ! ");
+        String NumeroAgencia = sc.nextLine();
+        
+        System.out.println("Por favor, digite o número da sua conta !");
+        int NumeroConta = sc.nextInt();
+       
+        System.out.println("Por favor, digite seu saldo ! ");
+        float Saldo = sc.nextFloat();
+        
+        System.out.println("Olá " + Name + ", obrigado por criar uma conta em nosso banco, sua agência é " +
+        NumeroAgencia + ", conta " + NumeroConta + " e seu saldo " + Saldo + " já está disponível para saque.");
+
+
     }
 }
